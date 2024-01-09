65e079f43942a0fc572e7ad2b4dadcce217f6133
M0H1K4N <nejer911@gmail.com>
2024-01-09 22:40:09 +0400

    I have punnyCode and trying to fix it

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -1,5 +1,17 @@
-function connectToDb() {
-  console.log("connectToDb");
+const mongoose = require("mongoose");
+
+// Load env variables
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Connected To DB");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // To export the function
