86fba287d10731797023bb01aa62b1fdf335e38b
M0H1K4N <nejer911@gmail.com>
2024-01-12 22:11:00 +0400

    finished for today

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -8,20 +8,23 @@ function App() {
     title: "",
     body: "",
   });
+  const [updateForm, setUpdateForm] = useState({
+    _id: null,
+    title: "",
+    body: "",
+  });
 
-  // useEffect
+  // Use effect
   useEffect(() => {
     fetchNotes();
   }, []);
 
-  // Functions
   const fetchNotes = async () => {
-    // Fetch notes
+    // Fetch the notes
     const res = await axios.get("http://localhost:3000/notes");
+
     // Set to state
-    console.log(res);
     setNotes(res.data.notes);
-    console.log(res);
   };
 
   const updateCreateFormField = (e) => {
@@ -40,28 +43,104 @@ function App() {
 
     setNotes([...notes, res.data.note]);
 
-    // Clear form state
-    setCreateForm({ title: "", body: "" });
+    setCreateForm({
+      title: "",
+      body: "",
+    });
+  };
+
+  const deleteNote = async (_id) => {
+    // Delete the note
+    const res = await axios.delete(`http://localhost:3000/notes/${_id}`);
+
+    // Update state
+    const newNotes = [...notes].filter((note) => {
+      return note._id !== _id;
+    });
+
+    setNotes(newNotes);
+  };
+
+  const handleUpdateFieldChange = (e) => {
+    const { value, name } = e.target;
+
+    setUpdateForm({
+      ...updateForm,
+      [name]: value,
+    });
+  };
+
+  const toggleUpdate = (note) => {
+    // Set state on update form
+    setUpdateForm({ title: note.title, body: note.body, _id: note._id });
+  };
+
+  const updateNote = async (e) => {
+    e.preventDefault();
+
+    const { title, body } = updateForm;
+
+    // Send the update request
+    const res = await axios.put(
+      `http://localhost:3000/notes/${updateForm._id}`,
+      { title, body }
+    );
+
+    // Update state
+    const newNotes = [...notes];
+    const noteIndex = notes.findIndex((note) => {
+      return note._id === updateForm._id;
+    });
+    newNotes[noteIndex] = res.data.note;
+
+    setNotes(newNotes);
+
+    // Clear update form state
+    setUpdateForm({
+      _id: null,
+      title: "",
+      body: "",
+    });
   };
 
   return (
-    <>
-    <div>
-      <div className="App">
-        <div>
-          <h2>Notes:</h2>
-          {notes && 
-          notes.map((note) =>{
-            return(
+    <div className="App">
+      <div>
+        <h2>Notes:</h2>
+        {notes &&
+          notes.map((note) => {
+            return (
               <div key={note._id}>
                 <h3>{note.title}</h3>
+                <button onClick={() => deleteNote(note._id)}>
+                  Delete note
+                </button>
+                <button onClick={() => toggleUpdate(note)}>Update note</button>
               </div>
-            )
+            );
           })}
-        </div>
+      </div>
 
+      {updateForm._id && (
+        <div>
+          <h2>Update note</h2>
+          <form onSubmit={updateNote}>
+            <input
+              onChange={handleUpdateFieldChange}
+              value={updateForm.title}
+              name="title"
+            />
+            <textarea
+              onChange={handleUpdateFieldChange}
+              value={updateForm.body}
+              name="body"
+            />
+            <button type="submit">Update note</button>
+          </form>
         </div>
+      )}
 
+      {!updateForm._id && (
         <div>
           <h2>Create note</h2>
           <form onSubmit={createNote}>
@@ -78,8 +157,8 @@ function App() {
             <button type="submit">Create note</button>
           </form>
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
 
