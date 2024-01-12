2a6caf17d93a10268ba2477208eeb1f00b3e4792
M0H1K4N <nejer911@gmail.com>
2024-01-12 19:15:18 +0400

    we can see now on the screen

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -7,16 +7,29 @@ function App() {
   useEffect(() => {
     fetchNotes();
   }, []);
-// 
-
+  //
 
   const fetchNotes = async () => {
     const res = await axios.get("http://localhost:3000/notes");
     console.log(res);
+    setNotes(res.data.notes);
+    console.log(res);
   };
   return (
     <>
-      <div>Hello Yleo! world ara chemi yle</div>
+      <div className="no1">
+        <div className="2">
+          <h2>Notes:</h2>
+          {notes &&
+            notes.map((note) => {
+              return (
+                <div>
+                  <h3>{note.title}</h3>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </>
   );
 }
