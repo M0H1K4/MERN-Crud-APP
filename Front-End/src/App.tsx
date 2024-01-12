5fca146947e8b6c99f483ae884545558fe2c9929
M0H1K4N <nejer911@gmail.com>
2024-01-12 19:06:27 +0400

    Add

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,14 +1,17 @@
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [notes, setNotes] = useState(null);
 
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   const fetchNotes = async () => {
     const res = await axios.get("http://localhost:3000/notes");
     console.log(res);
   };
-resizeByNotes(notes
   return (
     <>
       <div>Hello Yleo! world ara chemi yle</div>
