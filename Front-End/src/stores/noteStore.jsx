4f5815b48b068a89bec8f73b6c71865b73ffc723
M0H1K4N <nejer911@gmail.com>
2024-01-15 22:22:34 +0400

    add

--- Front-End/src/stores/noteStore.jsx
+++ Front-End/src/stores/noteStore.jsx
@@ -5,7 +5,7 @@ const notesStore = create((set) => ({
 
  fetchNotes: () =>{
   TODO: //continue from 5:01
-  // well today is done but tomorrow 
+  // well today is done but tomorrow I will 
  }
 }))
 
